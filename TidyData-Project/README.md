230a7c0351cf09e8494754d55f90ae408029b1d5
byates3 <byates3@nd.edu>
2025-03-18 01:16:01 -0400

    Portfolio update 2 updated

---
+++ TidyData-Project/README.md
@@ -0,0 +1,35 @@
+**Project overview**
+
+The goal of this project is to take a messy dataset, including missing values, variables sharing columns, etc., and transforming it into neat, easy to work with data. This will be done through a number of different methods including creating new tables, creating and reordering columns, and visualizing the data. Moreover, this project aims to demonstrate the importance of data tidying. Information on data tidying can be found under the "References Used" section. 
+
+This project can be understood from the Python file alone, but for stronger data visualization, I recommend launching this project in Streamlit. 
+-------------------------------------------------------------------------
+
+**Dataset Description**
+
+The data used describes the value of X-Men comics from the 1960s through the 1990s according to four different sellers: Heritage, eBay, Wiz, and oStreet. 
+-------------------------------------------------------------------------
+
+**Instructions**
+Download the mutant_moneyball.csv file found in the project folder. It must run alongside the Python code. 
+
+To launch this project in streamlit, navigate to the terminal, cd to the TidyData-Project folder, and run Streamlit for the TidyData.py file. 
+
+This project is dependant on four libraries:
+1. Pandas
+2. matplotlib.pyplot
+3. Seaborn
+4. Streamlit
+
+Ensure that all four libraries are imported correctly:
+1. pandas as pd
+2. matplotlib.pyplot as plt
+3. seaborn as sns
+4. streamlit as st
+-------------------------------------------------------------------------
+
+**References Used**
+This project uses data from the Mutant Moneyball dataset. This can be found at the following GitHub page: https://github.com/EliCash82/mutantmoneyball/tree/main
+
+The principles of tidy data can be found here: https://vita.had.co.nz/papers/tidy-data.pdf
+-------------------------------------------------------------------------
--- TidyData-Project/TidyData.py
+++ TidyData-Project/TidyData.py
@@ -3,11 +3,23 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
+# Streamlit app header
+st.header("Portfolio Update 2 - Beau Yates - Mutant Moneyball Data Manipulation and Visualization")
+st.divider()
+
+# A description of the aoo and its goals
+st.write("This app uses data from the mutant_moneyball.csv dataset. This data will be tidied up and visualized through bar plots and scatter plots.")
+st.divider()
+
+# Text introducing the original messy dataset as taken from Canvas
+st.subheader("The Original Dataset")
+st.write("The following table shows the orignal data as taken from Canvas. This includes missing data.")
+
 #_________________________________________________________________
 # Import the Mutant Moneyball dataset
 df = pd.read_csv("mutant_moneyball.csv")
 
-# Melt the dataframe
+# ----Melt the dataframe----
 # Original data columns are brought under the 'Value of Issues by Seller column
 # with each value being brought under the new 'Value' column
 df_melted = pd.melt(df,
@@ -22,7 +34,12 @@ df_melted = pd.melt(df,
 # Display the melted dataset in Streamlit
 st.dataframe(df_melted)
 
+st.write("The data is rather difficult to work with in this state as it is all in one table.")
+st.write("The 'Value of Issues by Seller column includes multiple variables: decade and seller. \
+         We will need to separate these variables and create new tables for each seller")
+st.divider()
 #_________________________________________________________________
+#----Creating new tables and separating the variables----
 
 # Create new columns for the decade and seller. Lines 23 and 24 were generated from ChatGPT 
 df_melted["Decade"] = df_melted["Value of Issues by Seller"].str.extract(r'(\d{2}s)')
@@ -46,42 +63,146 @@ df_oStreet = df_oStreet.dropna()
 #_________________________________________________________________
 # ----Heritage Seller data and visualizations----
 
+# Set header for Heritage seller section in Streamlit
+st.subheader("Values from Heritage Seller by Decade")
+
+# Introduce the cleaned up table
+st.write("The following table as split up the seller and decade variables into their own columns")
+st.write("Missing data has also been removed.")
+
 # Display the table in Streamlit
 st.dataframe(df_heritage)
 
-# Create a histogram of the data
-st.subheader("Heritage")
+# Introduce the data visualization
+st.divider()
+st.write("Now we can visualize the data clearly. To do this we will be using bar plots and scatter plots")
+
+# Create a bar plot of the Heritage seller data
 fig, ax = plt.subplots()
-sns.histplot(df_heritage, kde = True)
+sns.barplot(x="Decade", y="Value", data = df_heritage)
 
 # Label the visualization: title and axis labels
-ax.set_title("Total Value by Decade - Heritage Seller")
+ax.set_title("Average Value by Decade - Heritage Seller")
 ax.set_xlabel("Decade")
-ax.set_ylabel("Total Value")
+ax.set_ylabel("Value")
+
+# Display the bar plot in Streamlit
+st.pyplot(fig)
+
+# Create a scatter plot of the Heritage seller data
+fig, ax = plt.subplots()
+sns.scatterplot(x="Decade", y="Value", data = df_heritage)
+
+# Label the visualization: title and axis labels
+ax.set_title("Individual Values by Decade - Heritage Seller")
+ax.set_xlabel("Decade")
+ax.set_ylabel("Value")
+
+# Display the scatter plot in Streamlit
 st.pyplot(fig)
 
 #_________________________________________________________________
 
-# Create the plot
-fig, ax = plt.subplots()
-sns.barplot(x="Decade", y="Value", data=df_heritage)
+# Set up the remaining three datasets and their visualizations
+st.divider()
+st.write("The data will be presented in the same way for the other three sellers. Please see below.")
+st.divider()
 
-# Add titles and labels
-ax.set_title("Total Value by Decade - Heritage Seller")
-ax.set_xlabel("Decade")
-ax.set_ylabel("Total Value")
+#_________________________________________________________________
+# ----eBay Seller data and visualizations----
 
-# Display the plot in Streamlit
-st.pyplot(fig)
+# Set header for eBay seller section in Streamlit
+st.subheader("Values from eBay Seller by Decade")
 
-fig, ax = plt.subplots()
-sns.scatterplot(x="Decade", y="Value", data = df_heritage)
-st.pyplot(fig)
-
-
-
-
-
+# Display the table in Streamlit
 st.dataframe(df_ebay)
+
+# Create a bar plot of the eBay seller data
+fig, ax = plt.subplots()
+sns.barplot(x="Decade", y="Value", data = df_ebay)
+
+# Label the visualization: title and axis labels
+ax.set_title("Average Value by Decade - eBay Seller")
+ax.set_xlabel("Decade")
+ax.set_ylabel("Value")
+
+# Display the bar plot in Streamlit
+st.pyplot(fig)
+
+# Create a scatter plot of the eBay seller data
+fig, ax = plt.subplots()
+sns.scatterplot(x="Decade", y="Value", data = df_ebay)
+
+# Label the visualization: title and axis labels
+ax.set_title("Individual Values by Decade - eBay Seller")
+ax.set_xlabel("Decade")
+ax.set_ylabel("Value")
+
+# Display the scatter plot in Streamlit
+st.pyplot(fig)
+
+#_________________________________________________________________
+# ----Wiz Seller data and visualizations----
+st.divider()
+# Set header for Wiz seller section in Streamlit
+st.subheader("Values from Wiz Seller by Decade")
+
+# Display the table in Streamlit
 st.dataframe(df_wiz)
+
+# Create a bar plot of the Wiz seller data
+fig, ax = plt.subplots()
+sns.barplot(x="Decade", y="Value", data = df_wiz)
+
+# Label the visualization: title and axis labels
+ax.set_title("Average Value by Decade - Wiz Seller")
+ax.set_xlabel("Decade")
+ax.set_ylabel("Value")
+
+# Display the bar plot in Streamlit
+st.pyplot(fig)
+
+# Create a scatter plot of the Wiz seller data
+fig, ax = plt.subplots()
+sns.scatterplot(x="Decade", y="Value", data = df_wiz)
+
+# Label the visualization: title and axis labels
+ax.set_title("Individual Values by Decade - Wiz Seller")
+ax.set_xlabel("Decade")
+ax.set_ylabel("Value")
+
+# Display the scatter plot in Streamlit
+st.pyplot(fig)
+
+#_________________________________________________________________
+# ----oStreet Seller data and visualizations----
+st.divider()
+# Set header for oStreet seller section in Streamlit
+st.subheader("Values from oStreet Seller by Decade")
+
+# Display the table in Streamlit
 st.dataframe(df_oStreet)
+
+# Create a bar plot of the oStreet seller data
+fig, ax = plt.subplots()
+sns.barplot(x="Decade", y="Value", data = df_oStreet)
+
+# Label the visualization: title and axis labels
+ax.set_title("Average Value by Decade - oStreet Seller")
+ax.set_xlabel("Decade")
+ax.set_ylabel("Value")
+
+# Display the bar plot in Streamlit
+st.pyplot(fig)
+
+# Create a scatter plot of the oStreet seller data
+fig, ax = plt.subplots()
+sns.scatterplot(x="Decade", y="Value", data = df_oStreet)
+
+# Label the visualization: title and axis labels
+ax.set_title("Individual Values by Decade - oStreet Seller")
+ax.set_xlabel("Decade")
+ax.set_ylabel("Value")
+
+# Display the scatter plot in Streamlit
+st.pyplot(fig)
